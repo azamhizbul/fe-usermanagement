4d19d72e6c90ecc3ffd83c929a80e1a5bae41c73
azamhizbul <azamhizbul@gmail.com>
2022-09-01 11:01:09 +0700

    devops

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
   env: {
-    TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuaXBwb3MiOiI5NzIzMjU5MzEiLCJlbWFpbCI6Ijk3MjMyNTkzMUBwb3NpbmRvbmVzaWEuY28uaWQiLCJleHAiOjE2NjE1ODQwMjN9.oW9QFznBQVdtP7vPpHoJV24xKeX2XaYz7gV5hhkMjLc',
-    URLUSERMANAGE : 'http://localhost:8001'
+    TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuaXBwb3MiOiI5NzIzMjU5MzEiLCJlbWFpbCI6Ijk3MjMyNTkzMUBwb3NpbmRvbmVzaWEuY28uaWQiLCJleHAiOjE2NjIwOTEyMDN9.HKBDWTPdOWYYYaLn7N9nPls0_KdhZi1k2tWAL4C9jgc',
+    URLUSERMANAGE : 'http://20.198.213.153:8001'
   },
 }
 
